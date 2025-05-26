01c86e16b2dae682bc76760124a649fc86df62bc
Heng Damrong <khmerdamrong107@gmail.com>
2025-05-26 20:51:00 +0700

    Change php require

--- admin-panel/admins/login-admins.php
+++ admin-panel/admins/login-admins.php
@@ -1,5 +1,5 @@
-<?php require "../layouts/header.php"; ?>
-<?php require "../../config/config.php"; ?>
+<?php require __DIR__ . "/../layouts/header.php"; ?>
+<?php require __DIR__ . "/../config/config.php"; ?>
 
 
 <?php 
