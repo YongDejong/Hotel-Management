c9f59a5efeaa09cdc6323e638749de39004ab94c
Heng Damrong <khmerdamrong107@gmail.com>
2025-05-26 20:59:54 +0700

    Add Javascript Bootstrap

--- includes/header.php
+++ includes/header.php
@@ -92,3 +92,10 @@
 	    </div>
 	  </nav>
     <!-- END nav -->
+
+	<!-- Required JS (Bootstrap 4 + jQuery + Popper.js) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  </body>
+</html>
