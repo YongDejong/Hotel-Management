108c7f05a11706f54cdca6a1321fa819bd08893b
YongDejong <khmerdamrong107@gmail.com>
2025-05-31 14:54:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 define("HOST", "sql105.byethost31.com");
 define("DBNAME", "b31_39012238_hotel_management");
 define("USER", "b31_39012238");
-define("PASSWORD", "");
+define("PASSWORD", "ThreeEmpires0716986872");
 
 try {
     $conn = new PDO("mysql:host=".HOST.";dbname=".DBNAME, USER, PASSWORD);
