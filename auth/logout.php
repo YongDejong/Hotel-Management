0b7d573baf34038b8b22aff81a51a9c8440036f9
YongDejong <khmerdamrong107@gmail.com>
2025-05-31 14:31:29 +0700

    Delete auth directory

--- auth/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-
-    session_start();
-    session_unset(); // Unset all session variables
-    session_destroy(); // Destroy the session
-
-    header("location: http://localhost/hotel-booking"); // Redirect to login page
